a2a94fad390c59fa509c027670b9cf0cc9544dfa
Emir Acar <105319468+EmirAcar06@users.noreply.github.com>
2025-03-09 17:37:30 +0300

    Ocak4

---
+++ Görev/Görev/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.Write("Bir sayı girin: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int toplam = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            toplam += i;
+        }
+
+        Console.WriteLine("Toplam: " + toplam);
+    }
+}
