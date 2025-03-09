d3c602cbd27ebc359af75c2078a7f83c1eca9db9
Emir Acar <105319468+EmirAcar06@users.noreply.github.com>
2025-03-09 18:09:19 +0300

    Ocak3

---
+++ Görev2/Görev/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı girin: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        if (sayi % 2 == 0)
+        {
+            Console.WriteLine("Girilen sayı çifttir.");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı tektir.");
+        }
+
+        Console.Write("Bir sayı girin: ");
+        int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+        if (sayi1 > 0)
+        {
+            Console.WriteLine("Girilen sayı pozitiftir.");
+        }
+        else if (sayi1 < 0)
+        {
+            Console.WriteLine("Girilen sayı negatiftir.");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı sıfırdır.");
+        }
+
+
+    }
+}
