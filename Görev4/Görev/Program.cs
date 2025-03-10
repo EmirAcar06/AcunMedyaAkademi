7296306b5c0455e97042f25df61a99d5a4b7b296
Emir Acar <105319468+EmirAcar06@users.noreply.github.com>
2025-03-10 12:16:46 +0300

    Ocak1

---
+++ Görev4/Görev/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Kullanıcıdan adını al
+        Console.Write("Lütfen adınızı girin: ");
+        string name = Console.ReadLine();
+
+        // Kullanıcının adını ekrana yazdır
+        Console.WriteLine($"\nMerhaba, {name}!");
+    }
+}
