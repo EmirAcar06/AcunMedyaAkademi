b1b72546ba1b5c713f638f8aac05832e6cf6146f
Emir Acar <105319468+EmirAcar06@users.noreply.github.com>
2025-03-10 12:08:52 +0300

    Ocak2

---
+++ Görev3/Görev/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Kullanıcıdan ilk sayıyı al
+        Console.Write("Birinci sayıyı girin: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        // Kullanıcıdan ikinci sayıyı al
+        Console.Write("İkinci sayıyı girin: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        // İki sayının toplamını hesapla
+        double sum = num1 + num2;
+
+        // Sonucu ekrana yazdır
+        Console.WriteLine($"\nGirilen sayıların toplamı: {sum}");
+    }
+}
